999237f0ed572255679a4868d5b0195ac4a61f06
CarlosMena01 <cmena@unal.edu.co>
2023-08-05 18:17:17 -0500

    [IMPROVE] Best results in params

--- utils/config.py
+++ utils/config.py
@@ -2,7 +2,7 @@ SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 
 agents_len = 100
-random_percent = 5
-childrens_percent = 45
-copy_percent = 25
-muted_percent = 20
+random_percent = 0
+childrens_percent = 40
+copy_percent = 10
+muted_percent = 45
