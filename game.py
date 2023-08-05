c12688941c4867a7b6142238d229fcea2afa4ab3
CarlosMena01 <cmena@unal.edu.co>
2023-08-05 11:15:13 -0500

    [DOC] Comments in game code

--- game.py
+++ game.py
@@ -1,21 +1,23 @@
+# Import necessary libraries
 from utils.config import *
 import os
 import random
 from src.NN import Agent
 import numpy as np
-
 import pygame
 
+# Initialize the pygame library
 pygame.init()
 
-# Global Constants
+# Initialize the game window
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Chrome Dino Runner")  # Set window title
 
-pygame.display.set_caption("Chrome Dino Runner")
-
+# Load and set the game icon
 Ico = pygame.image.load("assets/DinoWallpaper.png")
 pygame.display.set_icon(Ico)
 
+# Load various game assets
 RUNNING = [
     pygame.image.load(os.path.join("assets/Dino", "DinoRun1.png")),
     pygame.image.load(os.path.join("assets/Dino", "DinoRun2.png")),
@@ -54,8 +56,9 @@ class Dinosaur:
     Y_POS_DUCK = 340
 
     def __init__(self, agent: Agent = None):
+        # Initialize the dinosaur's attributes
         self.X_POS = int(20 + 80*random.random())
-        if agent == None:
+        if agent is None:
             self.agent = Agent()
         else:
             self.agent = agent
@@ -75,6 +78,7 @@ class Dinosaur:
         self.dino_rect.y = self.Y_POS
 
     def update(self, obstacles):
+        # Update the dinosaur's behavior based on the agent's predictions
         if self.dino_duck:
             self.duck()
         if self.dino_run:
@@ -87,11 +91,13 @@ class Dinosaur:
 
         actions = [0, 0]
         if len(obstacles) > 0:
+            # Prepare input data for the agent's prediction
             data = [obstacles[0].rect.x, obstacles[0].rect.y,
-                    obstacles[0].rect.height,  obstacles[0].rect.width, self.dino_rect.x, self.dino_rect.y, game_speed]
+                    obstacles[0].rect.height, obstacles[0].rect.width, self.dino_rect.x, self.dino_rect.y, game_speed]
             data = np.asarray(data)
             actions = self.agent.predict(data)
 
+        # Update the dinosaur's behavior based on the predicted actions
         if (actions[0] == 1) and not self.dino_jump:
             self.dino_duck = False
             self.dino_run = False
@@ -100,12 +106,14 @@ class Dinosaur:
             self.dino_duck = True
             self.dino_run = False
             self.dino_jump = False
-        elif (sum(actions) == 0 or sum(actions) == 2) and not self.dino_jump:  # Both are zero or one
+        elif (sum(actions) == 0 or sum(actions) == 2) and not self.dino_jump:
+            # If no specific action is predicted or both actions are predicted, run
             self.dino_duck = False
             self.dino_run = True
             self.dino_jump = False
 
     def duck(self):
+        # Update the dinosaur's image and position when ducking
         self.image = self.duck_img[self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
@@ -113,6 +121,7 @@ class Dinosaur:
         self.step_index += 1
 
     def run(self):
+        # Update the dinosaur's image and position when running
         self.image = self.run_img[self.step_index // 5]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = self.X_POS
@@ -120,6 +129,7 @@ class Dinosaur:
         self.step_index += 1
 
     def jump(self):
+        # Define a jump function and update the dinosaur's image and position during the jump
         def f(x: float):
             return -x*(x-1)*94*4*2
         self.image = self.jump_img
@@ -132,44 +142,54 @@ class Dinosaur:
             self.dino_rect.y = self.Y_POS
 
     def draw(self, SCREEN):
+        # Draw the dinosaur's current image on the screen
         SCREEN.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
 
 
 class Cloud:
     def __init__(self):
+        # Initialize cloud's attributes
         self.x = SCREEN_WIDTH + random.randint(800, 1000)
         self.y = random.randint(50, 100)
         self.image = CLOUD
         self.width = self.image.get_width()
 
     def update(self):
+        # Update cloud's position based on game speed
         self.x -= game_speed
         if self.x < -self.width:
+            # Reset cloud's position if it goes off-screen
             self.x = SCREEN_WIDTH + random.randint(2500, 3000)
             self.y = random.randint(50, 100)
 
     def draw(self, SCREEN):
+        # Draw the cloud on the screen
         SCREEN.blit(self.image, (self.x, self.y))
 
 
 class Obstacle:
     def __init__(self, image, type):
+        # Initialize obstacle's attributes
         self.image = image
         self.type = type
         self.rect = self.image[self.type].get_rect()
         self.rect.x = SCREEN_WIDTH
 
     def update(self):
+        # Update obstacle's position based on game speed
         self.rect.x -= game_speed
         if self.rect.x < -self.rect.width:
+            # Remove obstacle from list if it goes off-screen
             obstacles.pop()
 
     def draw(self, SCREEN):
+        # Draw the obstacle on the screen
         SCREEN.blit(self.image[self.type], self.rect)
 
 
 class SmallCactus(Obstacle):
     def __init__(self, image):
+        # Initialize small cactus obstacle
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
         self.rect.y = 325
@@ -177,6 +197,7 @@ class SmallCactus(Obstacle):
 
 class LargeCactus(Obstacle):
     def __init__(self, image):
+        # Initialize large cactus obstacle
         self.type = random.randint(0, 2)
         super().__init__(image, self.type)
         self.rect.y = 300
@@ -186,12 +207,14 @@ class Bird(Obstacle):
     BIRD_HEIGHTS = [200, 250, 290, 320]
 
     def __init__(self, image):
+        # Initialize bird obstacle
         self.type = 0
         super().__init__(image, self.type)
         self.rect.y = random.choice(self.BIRD_HEIGHTS)
         self.index = 0
 
     def draw(self, SCREEN):
+        # Draw the bird animation frames on the screen
         if self.index >= 9:
             self.index = 0
         SCREEN.blit(self.image[self.index // 5], self.rect)
@@ -212,6 +235,7 @@ def main(players):
     obstacles = []
     pause = False
 
+    # Update the score and game speed
     def score():
         global points, game_speed
         points += 1
@@ -228,6 +252,7 @@ def main(players):
         textRect.center = (900, 40)
         SCREEN.blit(text, textRect)
 
+    # Update the background's scrolling
     def background():
         global x_pos_bg, y_pos_bg
         image_width = BG.get_width()
@@ -238,11 +263,13 @@ def main(players):
             x_pos_bg = 0
         x_pos_bg -= game_speed
 
+    # Unpause the game
     def unpause():
         nonlocal pause, run
         pause = False
         run = True
 
+    # Pause the game and display pause message
     def paused():
         nonlocal pause
         pause = True
@@ -272,21 +299,27 @@ def main(players):
 
         SCREEN.fill((255, 255, 255))
 
+        # Generate new obstacles if no obstacles are present
         if len(obstacles) == 0:
-            if random.randint(0, 2) == 0:
+            obstacle_type = random.randint(0, 2)
+            if obstacle_type == 0:
                 obstacles.append(SmallCactus(SMALL_CACTUS))
-            elif random.randint(0, 2) == 1:
+            elif obstacle_type == 1:
                 obstacles.append(LargeCactus(LARGE_CACTUS))
-            elif random.randint(0, 2) == 2:
+            else:
                 obstacles.append(Bird(BIRD))
 
+        # Update and draw players
         for player in players:
             player.draw(SCREEN)
             player.update(obstacles)
 
+        # Update and draw obstacles
         for obstacle in obstacles:
             obstacle.draw(SCREEN)
             obstacle.update()
+
+            # Check collision between players and obstacles
             for player in players:
                 if player.dino_rect.colliderect(obstacle.rect):
                     player.agent.score = points
@@ -298,13 +331,15 @@ def main(players):
                         pygame.time.delay(2000)
                         return death_players
 
+        # Update and draw background
         background()
 
+        # Update and draw cloud
         cloud.draw(SCREEN)
         cloud.update()
 
+        # Update score and manage frame rate
         score()
-
         clock.tick(30)
         pygame.display.update()
 
