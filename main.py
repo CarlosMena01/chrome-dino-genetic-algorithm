19839222fa086c339091872115211a37a55915b6
CarlosMena01 <cmena@unal.edu.co>
2023-08-06 20:03:03 -0500

    [ADD] Results file

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import numpy as np
 from src.NN import Agent
 from game import *
 from utils.config import *
+from datetime import date
+
+path = "./data/"
 
 
 def iteration(population, generation):
@@ -18,6 +21,16 @@ def iteration(population, generation):
         population[i] = dino.agent
     # Sort the agents based on their 'score' attribute in descending order
     population = sorted(population, key=lambda obj: obj.score, reverse=True)
+    # Write in a file the best score genomes
+    # extract current local date
+    today = date.today()
+    today = today.strftime("%d-%m-%Y")
+    file_name = f"{path}results_{today}.txt"
+    with open(file_name, "a") as f:
+        f.write(f"GENERATION {generation}\n")
+        for agent in population:
+            f.write(f"{agent.score} // {agent.chromosome.get_sequence()}\n")
+
     new_population = []
     # Reproduce to create the new population
     # The reproduction is random and can be just a copy, a mix, mute or combination
@@ -26,7 +39,7 @@ def iteration(population, generation):
         new_population.append(Agent())
     # The copies
     for i in range(int(agents_len*copy_percent/100)):
-        agent = Agent(population[i % 3].chromosome)
+        agent = Agent(population[i % 5].chromosome)
         new_population.append(agent)
     # The childrens
     for _ in range(int(agents_len*childrens_percent/100)):
